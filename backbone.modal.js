6776e368c5fac15e642f4a9f2bebf926f6cdc4c6
Makis Tracend <makis.tracend@gmail.com>
2013-02-06 17:12:44 -0800

    direct copy from gist:tracend/3446570

---
+++ backbone.modal.js
@@ -0,0 +1,88 @@
+// Backbone.js Modal extension
+//
+// Created by: Makis Tracend (@tracend)
+// Source: https://gist.github.com/3446570
+//
+// Licensed under the MIT license: http://makesites.org/license/mit
+
+(function(_, Backbone) {
+	
+	// conditioning the existance of the Backbone APP()
+	var View = ( APP ) ? APP.View : Backbone.View;
+	var Parent = ( APP ) ? APP.Views : Backbone;
+
+	Parent.Modal = View.extend({
+		el: "#popup",
+		template : {}, 
+		// events
+		events: {
+			"click input[type='submit']" : "clickSubmit",
+			"click .close" : "clickClose"
+		},
+		initialize: function(){
+			_.bindAll(this, 'setup', 'render', 'update', 'center', 'clickSubmit', 'clickClose');
+			// get the html fragment 
+			html = this.options.html || "popup.html";
+			
+			$.get(html, this.setup);
+		},
+		setup: function( template ){
+			var self = this;
+	
+			// loop through all the handlebar templates
+			$(template).filter("script").each(function(){
+				var id = $(this).attr('id');
+				self.template[id] = Handlebars.compile( $(this).html() );
+			});
+	
+		}, 
+		render: function(){
+			var self = this;
+			var html = this.template[this.type]( this.data.toJSON() );
+			$(this.el).html( html );
+			// post-processing
+			$(this.el).find("img").load(function(){
+				self.center();
+			});
+		}, 
+		update: function( data ){
+			this.data = data;
+			this.type = data.get("type");
+			// render the view
+			this.render();
+			// presentation updates
+			this.center();
+			
+			$(this.el).show();
+		},
+		// helpers
+		center: function(){
+			//
+			var width = $(this.el).width();
+			var height = $(this.el).height();
+			var top = document.body.scrollTop + (window.innerHeight/2);
+			$(this.el).css("top", top+"px");
+			$(this.el).css("margin-left", "-"+width/2+"px");
+			$(this.el).css("margin-top", "-"+height/2+"px");
+		}, 
+		// click triggers
+		clickSubmit: function( e ){
+			e.preventDefault();
+			
+			// process request...
+			
+			// after sending the request, close the panel
+			this.clickClose();
+			
+			return false;
+			
+		},
+		clickClose: function( e ){
+			e.preventDefault();
+			$(this.el).hide();
+			// update the view
+			return false;
+		}
+	});
+
+})(this._, this.Backbone);
---
+++ readme.md
@@ -0,0 +1,62 @@
+# Backbone.js Modal extension
+
+A simple way to extend your website content using modal containers
+
+## Dependendies
+
+* [Backbone.js](http://backbonejs.org/)
+* [Underscore.js](http://underscorejs.org/)
+* [Handlebars.js](http://handlebarsjs.com/)
+* [Backbone.APP](http://github.com/makesites/backbone-app)
+
+## Install
+
+Using Bower: 
+```
+bower install backbone.modal
+```
+
+## Usage
+
+After you include the script in your dependency stack the popup will be available to extend with your options. Example: 
+```
+APP.Views.SimpleModal = APP.Views.Modal.extend({
+	options: {
+		html: "/assets/html/simple.html"
+	}
+});
+```
+
+Note that the "#popup" container needs to be available in the dom before initializing the popup view. 
+
+It is advised that you only intantiate the popup view once and reuse the same instance for different content. For example: 
+```
+this.modal = new APP.Views.SimpleModal();
+
+//...and later in your code...
+
+this.modal.update(model)
+```
+
+
+## Options
+
+* **html** : the html fragment that contains all the popup views (in hanglebars templates) 
+
+## Methods
+
+In addition to adding your own methods, you can call or extend any of the following that are already available in the view
+
+* **update** : loads the popup with new data - a model or backbone collection is expected as the update data
+
+* **clickSubmit** : triggered with input submission buttons in the popup
+
+* **clickClose** : closes the popup container
+
+
+## Credits 
+
+Created by Makis Tracend ([@tracend](http://github.com/tracend)) for [Makesites.org](http://makesites.org)
+
+Distributed under the [MIT license](http://makesites.org/license/mit.txt)
+
