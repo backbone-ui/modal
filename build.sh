fd471af2ca2b24eb69dd34a6363034dbb874bfee
Makis Tracend <makis.tracend@gmail.com>
2015-11-12 02:06:35 -0800

    + build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+lessc -x assets/less/backbone.ui.modal.less assets/css/backbone.ui.modal.css
+lessc -x assets/less/backbone.ui.modal-style01.less assets/css/backbone.ui.modal-style01.css
+lessc -x assets/less/backbone.ui.modal-style02.less assets/css/backbone.ui.modal-style02.css
